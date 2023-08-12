76680ea156eb43c97e5e44b0a9b75213957b3462
Cauanz <61758981+Cauanz@users.noreply.github.com>
2023-08-12 12:42:12 -0300

    ✨ Making Main container

--- RacingPulse/src/pages/MainNews.jsx
+++ RacingPulse/src/pages/MainNews.jsx
@@ -3,7 +3,14 @@ import './MainNews.css'
 function MainNews() {
    return (
       <div className="MainNewsContainer">
-         
+         <div className='LeftContainer'>Left</div>
+         <div className='RightTopContainer'>
+            <div className='RightTopLeftContainer'>Right Top Left</div>
+            <div className='RightTopMidContainer'>Right Top Mid</div>
+            <div className='RightTopRightContainer'>Right Top Right</div>
+         </div>
+         <div className="RightMidContainer">Right Mid</div>
+         <div className="RightBottomContainer">Right Bottom</div>
       </div>
    )
 }
