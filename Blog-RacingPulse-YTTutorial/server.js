d22056fa64d0d5b22332634ee9c355dade6bf60d
Cauanz <61758981+Cauanz@users.noreply.github.com>
2024-03-30 16:56:26 -0300

    Adicionadas dependências marked e slugify

--- Blog-RacingPulse-YTTutorial/server.js
+++ Blog-RacingPulse-YTTutorial/server.js
@@ -1,20 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Article = require("./models/article");
 const articleRouter = require("./routes/articles");
 const app = express();
 
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/articles', articleRouter)
-
-
-app.get('/', (req, res) => {
-   const articles = [{
-      title: "Test Article",
-      createdAt: new Date(),
-      description: 'Test Description'
-   }]
-   res.render("index", { articles: articles })
+app.get('/', async (req, res) => {
+   const articles = await Article.find().sort({
+      createdAt: 'desc'
+   })
+   res.render("articles/index", { articles: articles })
 })
+
+app.use('/articles', articleRouter);
 
 app.listen(5173);
